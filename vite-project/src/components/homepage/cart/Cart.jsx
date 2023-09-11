95c5a893df30a42e4d8b74fb388488ac33c42501
Bryan Perez <bryanperez@Bryans-MacBook-Air.local>
2023-09-10 20:27:51 -0500

    cart now renders the users cart depending on who loggs in, but when you refresh it forgets who is logged in

--- vite-project/src/components/homepage/cart/Cart.jsx
+++ vite-project/src/components/homepage/cart/Cart.jsx
@@ -8,7 +8,8 @@ import { useState } from 'react'
 function Cart(props) {
   const userId = props.userId;
   const [cartItems, setCartItems] = useState([])
-
+  const [items, setItems] = useState([])
+//fetches the users cart
   useEffect(() => {
     async function renderUserCart() {
         try {
@@ -21,8 +22,26 @@ function Cart(props) {
         }
     }
     renderUserCart();
-}, [])
-  console.log(cartItems)
+}, [userId])
+
+//fetches all the items then sorts through them and updates the array so that the items that are in 
+//the cart will be desplayed on the screen
+  useEffect(() => {
+    async function renderAllItems() {
+        try {
+            const response = await fetch(`https://fakestoreapi.com/products`)
+            const result = await response.json()
+
+            const cartItemIds = cartItems.map(item => item.productId)
+
+            const filteredItems = result.filter(item => cartItemIds.includes(item.id))
+            setItems(filteredItems)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+    renderAllItems();
+  }, [cartItems])
   function CheckoutBtn () {
     const path = `/Checkout`
     navigate(path);
@@ -32,7 +51,22 @@ function Cart(props) {
       <div>
         <NavBar />
         <button onClick={CheckoutBtn}>Checkout</button>
-        <h1>Cart</h1>
+        <div className='main'> 
+                <div className='container'>
+                    <div className='row'>
+                        {
+                            items.map((item) => {
+                                return (
+                                    <div key={item.id} className='itemCard'>
+                                        <ul>{item.title}</ul>
+                                        <img src={item.image} alt='picture_of_item' className='item-img'/>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
       </div>
     )
   }
